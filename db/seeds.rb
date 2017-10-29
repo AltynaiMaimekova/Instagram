4c590c884e846dddbd58c57c65b03381e34648e6
Altynai Maimekova <janarbek.akinai@gmail.com>
2017-10-30 02:49:02 +0600

    Seeds were added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create!(user_name: 'Brad Pitt', email:'bradpitt@gmail.com', password: 'qwerty1', password_confirmation: 'qwerty1', bio: "Accountant, 30 years old. Born in London", avatar: File.new("app/assets/images/brad.jpg"))
+user2 = User.create!(user_name: 'Russell Crowe', email:'russell@gmail.com', password: 'qwerty2', password_confirmation: 'qwerty2', bio: "Finansist, 40 years old. Born in New York", avatar: File.new("app/assets/images/russell.jpg"))
+post1 = user1.posts.build(title: 'Best summer in my life', image: File.new("app/assets/images/summer.jpg"))
+post2 = user2.posts.build(title: 'My family', image: File.new("app/assets/images/family.jpg"))
+post1.save
+post2.save
